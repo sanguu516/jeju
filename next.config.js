b72c7a2357dd10312f51c6492e79d360ee37cfb8
lkd9125(이경도) <lkd9125@naver.com>
2024-03-14 18:55:05 +0900

    output 설정 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
-};
-module.exports = nextConfig;
+
 
 module.exports = {
+  output: 'export',
+
   images: {
     remotePatterns: [
       {
