6e1504b533cb96692044e985a83d457c35ddff4f
sanguu516 <>
2024-04-30 17:56:25 +0000

    구글 검색 엔진 노출 인증 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,26 +28,24 @@ export default function RootLayout({ children }: Props) {
     <ReactQueryProvider>
       <html lang='en' suppressHydrationWarning>
         <head>
-          <meta
+          {/* <meta
             name='google-site-verification'
             content='t7k90RyFLJRCBeCvhXQ1wiD6_mFnh-MmyPfp6hynDZ4'
-          />
+          /> */}
           <link rel='icon' href='/favicon.ico' sizes='any' />
-          <title>제주 여행 코스 - JEJU 제주여행 여행계획</title>
-          <meta property='og:type' content='website' />
           <meta property='og:url' content='http://jjeju.site' />
           <meta
             property='og:title'
             content='제주 여행 코스 - JEJU 제주여행 여행계획'
           />
-          <meta property='og:image' content='/main.png' />
           <meta
             property='og:description'
             content='제주 여행 코스를 짜고 여행을 계획할 수 있는 사이트입니다. 제주도의 다양한 관광명소와 여행 정보를 제공합니다.'
           />
+          <meta property='og:type' content='website' />
+          <meta property='og:image' content='http://jjeju.site/main.png' />
           <meta property='og:site_name' content='JEJU' />
           <meta property='og:locale' content='ko_KR' />
-
           <meta property='og:image:width' content='1200' />
           <meta property='og:image:height' content='630' />
 
